60c97f39e772ae790dab585424e614fd241f26e4
Emanuel Goncalves <emanuelgdesouza@gmail.com>
2017-09-11 14:14:42 -0300

    Removed import files

--- src/common/index.js
+++ src/common/index.js
@@ -6,5 +6,3 @@ export { default as Field } from './field'
 
 export { default as optionValueToVal } from './option-value-to-val'
 export { default as returnProps } from './return-props'
-export { default as returnPropsToArray } from './return-props-to-array'
-export { default as returnDefaultEvent } from './return-defaults-events'
